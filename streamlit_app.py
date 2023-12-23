953bf5405c5059ce1ca9602fc4a5fb197e31ccf2
Qazi Danish Ayub <qazidanish66@gmail.com>
2023-12-24 01:58:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,15 @@
 import streamlit as st
 from datetime import datetime
 
-# Custom CSS to inject into Streamlit's HTML
-st.markdown(
-    """
-    <style>
-    .stTextInput, .stDateInput {
-        margin-bottom: 10px;
-    }
-    /* You can add more styles to make it colorful and visually appealing */
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.title('Mortgage Application Form')
 
-st.title('Mortgage Application Form for Predicting the PurchaseByInvesterDT')
+# Use columns for a responsive layout
+num_cols = 3  # Adjust the number of columns based on device responsiveness
 
-# Input validation functions based on the field name
+# Dictionary to hold the form inputs
+input_data = {}
+
+# Function to validate input based on field name
 def validate_input(field_name, value):
     if 'Zip' in field_name and not value.isnumeric():
         return f"{field_name} should contain only numbers."
@@ -33,32 +26,27 @@ def validate_input(field_name, value):
     # Add more validations as needed
     return None
 
-# Responsive layout for input fields
-num_cols = 3  # You can adjust this based on the layout you want
-cols = st.beta_columns(num_cols)
-cols_iter = iter(cols)
-
+# Iterate over fields and create inputs with validation
 for index, field in enumerate(fields):
-    col = next(cols_iter)
+    # Create columns dynamically
+    col = st.columns(num_cols)[index % num_cols]
     with col:
         if "Date" in field:
             # Use date input for fields containing 'Date'
-            input_value = st.date_input(field, datetime.today())
+            input_data[field] = st.date_input(field, datetime.today())
         else:
             # Use text input for all other fields
-            input_value = st.text_input(field)
+            input_data[field] = st.text_input(field)
 
         # Perform validation and show error if needed
-        error_message = validate_input(field, input_value)
+        error_message = validate_input(field, input_data[field])
         if error_message:
             st.error(error_message)
 
 # Button to submit the form
-if st.button('Predict Purchase Date'):
-    # Assume all data is valid for demonstration purposes
-    st.success('Application Submitted Successfully!')
+if st.button('Submit'):
+    # Here you can handle the form submission, like saving the data to a database
     # For demonstration, the input data is printed on the screen
-    # Replace with actual form handling logic
-    st.json({field: col for field, col in zip(fields, cols)})
+    st.json(input_data)
 
 # To run this app, save the above code to a file and run it with: streamlit run your_script.py
