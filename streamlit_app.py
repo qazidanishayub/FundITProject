77f4cfd5cf01e766b23aea7550394e7782216fb8
Qazi Danish Ayub <qazidanish66@gmail.com>
2023-12-24 02:02:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,7 @@ from datetime import datetime
 
 st.title('Mortgage Application Form')
 
-# Use columns for a responsive layout
-num_cols = 3  # Adjust the number of columns based on device responsiveness
-
-# Dictionary to hold the form inputs
-input_data = {}
+# Input fields as specified
 fields = [
     "Investor",
     "SubmittedToInvestorDate",
@@ -40,28 +36,34 @@ fields = [
     "LockDate",
     "LoanProgram"
 ]
+
 # Function to validate input based on field name
 def validate_input(field_name, value):
+    errors = []
     if 'Zip' in field_name and not value.isnumeric():
-        return f"{field_name} should contain only numbers."
+        errors.append(f"{field_name} should contain only numbers.")
     if 'Rate' in field_name or 'Ratio' in field_name or 'APR' in field_name:
         try:
-            float(value)
+            value = float(value)
+            if value <= 0:
+                errors.append(f"{field_name} should be a positive number.")
         except ValueError:
-            return f"{field_name} should be a number."
-    if 'Date' in field_name:
-        try:
-            datetime.strptime(value, '%Y-%m-%d')
-        except ValueError:
-            return f"{field_name} should be a valid date in YYYY-MM-DD format."
-    # Add more validations as needed
-    return None
+            errors.append(f"{field_name} should be a number.")
+    # You can add more specific validations for each field as needed.
+    return errors
 
-# Iterate over fields and create inputs with validation
+# Create a responsive layout with 2-3 columns based on the width of the screen
+num_cols = 3
+cols = st.columns(num_cols)
+
+# Dictionary to hold the form inputs
+input_data = {}
+errors = {}
+
+# Create input fields with validation
 for index, field in enumerate(fields):
-    # Create columns dynamically
-    col = st.columns(num_cols)[index % num_cols]
-    with col:
+    col_index = index % num_cols
+    with cols[col_index]:
         if "Date" in field:
             # Use date input for fields containing 'Date'
             input_data[field] = st.date_input(field, datetime.today())
@@ -70,14 +72,19 @@ for index, field in enumerate(fields):
             input_data[field] = st.text_input(field)
 
         # Perform validation and show error if needed
-        error_message = validate_input(field, input_data[field])
-        if error_message:
-            st.error(error_message)
+        field_errors = validate_input(field, input_data[field])
+        if field_errors:
+            errors[field] = field_errors
+            for err in field_errors:
+                st.error(err)
 
 # Button to submit the form
 if st.button('Submit'):
-    # Here you can handle the form submission, like saving the data to a database
-    # For demonstration, the input data is printed on the screen
-    st.json(input_data)
+    if not errors:
+        st.success('Application Submitted Successfully!')
+        # For demonstration, the input data is printed on the screen
+        st.json(input_data)
+    else:
+        st.error('Please correct the errors before submitting.')
 
 # To run this app, save the above code to a file and run it with: streamlit run your_script.py
