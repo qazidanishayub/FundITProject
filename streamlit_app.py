670a9e7683522e3b6524781ec21ba9a38210b27b
Qazi Danish Ayub <qazidanish66@gmail.com>
2023-12-24 01:54:11 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,64 @@
 import streamlit as st
 from datetime import datetime
 
-st.title('Mortgage Application Form')
+# Custom CSS to inject into Streamlit's HTML
+st.markdown(
+    """
+    <style>
+    .stTextInput, .stDateInput {
+        margin-bottom: 10px;
+    }
+    /* You can add more styles to make it colorful and visually appealing */
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-# Dictionary to hold the form inputs
-input_data = {}
+st.title('Mortgage Application Form for Predicting the PurchaseByInvesterDT')
 
-# Define the fields with the 'Date' in their names as date input fields
-# Other fields will be considered as text input fields
-fields = [
-    "Investor",
-    "SubmittedToInvestorDate",
-    "propertyCounty",
-    "APR",
-    "CashoutToBorrower",
-    "FundingDate",
-    "DTI",
-    "ModifiedTimestamp",
-    "FICO",
-    "QualifyingHousingRatio",
-    "PropertyZip",
-    "PrimaryBorrowerCity",
-    "TotalPrice",
-    "PropertyCity",
-    "TotalLoanAmount",
-    "PrimaryBorrowerZip",
-    "AppraisalValue",
-    "CostCenter",
-    "TotalPayment",
-    "CLTV",
-    "Branch",
-    "InterestRate",
-    "LockExpirationDate",
-    "CreateDate",
-    "LTV",
-    "PlannedClosingDate",
-    "Points",
-    "Region",
-    "LockDate",
-    "LoanProgram"
-]
+# Input validation functions based on the field name
+def validate_input(field_name, value):
+    if 'Zip' in field_name and not value.isnumeric():
+        return f"{field_name} should contain only numbers."
+    if 'Rate' in field_name or 'Ratio' in field_name or 'APR' in field_name:
+        try:
+            float(value)
+        except ValueError:
+            return f"{field_name} should be a number."
+    if 'Date' in field_name:
+        try:
+            datetime.strptime(value, '%Y-%m-%d')
+        except ValueError:
+            return f"{field_name} should be a valid date in YYYY-MM-DD format."
+    # Add more validations as needed
+    return None
 
-for field in fields:
-    if "Date" in field:
-        # Use date input for fields containing 'Date'
-        input_data[field] = st.date_input(field, datetime.today())
-    else:
-        # Use text input for all other fields
-        input_data[field] = st.text_input(field)
+# Responsive layout for input fields
+num_cols = 3  # You can adjust this based on the layout you want
+cols = st.beta_columns(num_cols)
+cols_iter = iter(cols)
+
+for index, field in enumerate(fields):
+    col = next(cols_iter)
+    with col:
+        if "Date" in field:
+            # Use date input for fields containing 'Date'
+            input_value = st.date_input(field, datetime.today())
+        else:
+            # Use text input for all other fields
+            input_value = st.text_input(field)
+
+        # Perform validation and show error if needed
+        error_message = validate_input(field, input_value)
+        if error_message:
+            st.error(error_message)
 
 # Button to submit the form
-if st.button('Submit'):
+if st.button('Predict Purchase Date'):
+    # Assume all data is valid for demonstration purposes
     st.success('Application Submitted Successfully!')
-    # Here you can handle the form submission, like saving the data to a database
     # For demonstration, the input data is printed on the screen
-    st.json(input_data)
+    # Replace with actual form handling logic
+    st.json({field: col for field, col in zip(fields, cols)})
 
 # To run this app, save the above code to a file and run it with: streamlit run your_script.py
